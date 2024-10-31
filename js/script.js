f1acea842ddbf7772077643aed05303921e09b52
inatecsolution <douglasfm8@hotmail.com>
2024-10-30 18:05:00 -0600

    conversion tostring

--- js/script.js
+++ js/script.js
@@ -21,10 +21,11 @@ let interval = setInterval(function () {
     let seconds = Math.floor((distance % (1000 * 60)) / (1000));
 
     //Escribimos resultados
-    $days.innerHTML = days;
-    $hours.innerHTML = hours;
-    $minutes.innerHTML = minutes;
-    $seconds.innerHTML = ('0' + seconds).slice(-2);
+    $days.innerHTML = days.toString();
+    $hours.innerHTML = hours.toString();
+    $minutes.innerHTML = minutes.toString();
+    $seconds.innerHTML = seconds.toString();
+    //$seconds.innerHTML = ('0' + seconds).slice(-2);
 
     //Cuando llegue a 0
     if (distance < 0) {
