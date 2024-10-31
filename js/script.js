c23df09e386b6fb99d40a6f16f21a44fe9d1f92a
inatecsolution <douglasfm8@hotmail.com>
2024-10-31 07:42:05 -0600

    Cambio script countdown

--- js/script.js
+++ js/script.js
@@ -1,39 +1,50 @@
-const $days = document.getElementById('days'),
-    $hours = document.getElementById('hours'),
-    $minutes = document.getElementById('minutes'),
-    $seconds = document.getElementById('seconds'),
-    $finalMessage = document.querySelector('.final-sms');
+let dayBox = document.getElementById("days");
+let hrBox = document.getElementById("hours");
+let minBox = document.getElementById("minutes");
+let secBox = document.getElementById("seconds");
 
-//Fecha a futuro
-const countdownDate = new Date('11 09, 2024 18:00:00').getTime();
+//Format: Date(year, month, day, hour, minute)
+//Year is counter from 0 to 11
+let endDate = new Date(2024, 10, 9, 7, 35);
+//Output value in milliseconds
+let endTime = endDate.getTime();
+function countdown() {
+  let todayDate = new Date();
+  //Output value in milliseconds
+  let todayTime = todayDate.getTime();
 
-let interval = setInterval(function () {
-    //Obtener fecha actual y milisegundos
-    const now = new Date().getTime();
-
-    //Obtener las distancias entre ambas fechas
-    let distance = countdownDate - now;
-
-    //Calculos a dias-horas-minutos-segundos
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / (1000));
-
-    //Escribimos resultados
-    $days.innerHTML = days.toString();
-    $hours.innerHTML = hours.toString();
-    $minutes.innerHTML = minutes.toString();
-    $seconds.innerHTML = seconds.toString();
-    //$seconds.innerHTML = ('0' + seconds).slice(-2);
-
-    //Cuando llegue a 0
-    if (distance < 0) {
-        clearInterval(interval);
-        $finalMessage.style.transform = 'translateY(0)';
-    }
-}, 1000);
-
+  let remainingTime = endTime - todayTime;
+  //60sec => 1000 milliseconds
+  let oneMin = 60 * 1000;
+  //1hr => 60 minutes
+  let oneHr = 60 * oneMin;
+  //1 day => 24 hours
+  let oneDay = 24 * oneHr;
+  //Function to format number if it is single digit
+  let addZeroes = (num) => (num < 10 ? `0${num}` : num);
+  //If end dat is before today date
+  if (endTime < todayTime) {
+    clearInterval(i);
+    document.querySelector(
+      ".countdown"
+    ).innerHTML = `<h1>Countdown had expired!</h1>`;
+  }
+  //If end date is not before today date
+  else {
+    //Calculating remaining days, hrs,mins ,secs
+    let daysLeft = Math.floor(remainingTime / oneDay);
+    let hrsLeft = Math.floor((remainingTime % oneDay) / oneHr);
+    let minsLeft = Math.floor((remainingTime % oneHr) / oneMin);
+    let secsLeft = Math.floor((remainingTime % oneMin) / 1000);
+    //Displaying Valurs
+    dayBox.textContent = addZeroes(daysLeft);
+    hrBox.textContent = addZeroes(hrsLeft);
+    minBox.textContent = addZeroes(minsLeft);
+    secBox.textContent = addZeroes(secsLeft);
+  }
+}
+let i = setInterval(countdown, 1000);
+countdown();
 
 
 //función cargar modal
